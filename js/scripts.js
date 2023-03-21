c8ddba441d4054cec3a25a29c7dcb7144f393178
Josh Klein <skiz303@gmail.com>
2023-03-21 00:05:04 -0400

    cleaned up js and bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,293 +1,309 @@
-document.querySelector("#calc").onclick = doCalc;
+document.querySelector("#calc").onclick = doCalc
 
 function doCalc() {
-    let charClass = document.querySelector("#charClass").value;
-    let charLevel = document.querySelector("#charLevel").value;
-    let charStr = document.querySelector("#charStr").value;
-    let charHaste = document.querySelector("#charHaste").value;
-    let skillDW = document.querySelector("#skillDW").value;
-    let skillDA = document.querySelector("#skillDA").value;
-    let skillOff = document.querySelector("#skillOff").value;
-    let skillBS = document.querySelector("#skillBS").value;
-    let mhDmg = document.querySelector("#mhDmg").value;
-    let mhDelay = document.querySelector("#mhDelay").value;
-    let ohDmg = document.querySelector("#ohDmg").value;
-    let ohDelay = document.querySelector("#ohDelay").value;
-    let thBonus = document.querySelector("#thBonus").value;
+    let charClass = document.querySelector("#charClass").value
+    let charLevel = document.querySelector("#charLevel").value
+    let charStr = document.querySelector("#charStr").value
+    let charHaste = document.querySelector("#charHaste").value
+    let skillDW = document.querySelector("#skillDW").value
+    let skillDA = document.querySelector("#skillDA").value
+    let skillOff = document.querySelector("#skillOff").value
+    let skillBS = document.querySelector("#skillBS").value
+    let mhDmg = document.querySelector("#mhDmg").value
+    let mhDelay = document.querySelector("#mhDelay").value
+    let ohDmg = document.querySelector("#ohDmg").value
+    let ohDelay = document.querySelector("#ohDelay").value
+    let thBonus = document.querySelector("#thBonus").value
 
-    let dmgMod = getDmgMod(skillOff, charStr);
-    let dmgBonus = getDmgBonus(charLevel, thBonus, charClass);
-    let dwChance = getDwChance(charLevel, skillDW, charClass);
-    let daChance = getDaChance(skillDA);
-    let taChance = getTaChance(charLevel, skillDA, charClass);
+    let dmgMod = getDmgMod()
+    let dmgBonus = getDmgBonus()
+    let hasteCap = getHasteCap()
+    let dwChance = getDwChance()
+    let daChance = getDaChance()
+    let taChance = getTaChance()
 
-    let mhDmgCapO = getDmgCap(charLevel, mhDmg, charClass);
-    let mhMinO = getMainMin(dmgBonus);
-    let mhMaxO = getMainMax(dmgMod, mhDmg, thBonus, dmgBonus);
-    let mhDelayO = getWepDelay(mhDmg, mhDelay, charHaste, charLevel);
-    let mhDpsO = getMainDPS(mhMinO, mhMaxO, mhDelayO);
+    let mhDmgCapO = getDmgCap(mhDmg)
+    let mhMinO = getMainMin()
+    let mhMaxO = getMainMax()
+    let mhDelayO = getWepDelay(mhDmg, mhDelay)
+    let mhDpsO = getMainDPS()
 
-    let ohDmgCapO = getDmgCap(charLevel, ohDmg, charClass);
-    let ohMinO = getOffMin(ohDmg);
-    let ohMaxO = getOffMax(ohDmg, dmgMod);
-    let ohDelayO = getWepDelay(ohDmg, ohDelay, charHaste, charLevel);
-    let ohDpsO = getOffDPS(ohMinO, ohMaxO, ohDelayO, dwChance);
+    let ohDmgCapO = getDmgCap(ohDmg)
+    let ohMinO = getOffMin()
+    let ohMaxO = getOffMax()
+    let ohDelayO = getWepDelay(ohDmg, ohDelay)
+    let ohDpsO = getOffDPS()
 
-    let ttlDpsO = getTtlDPS(charLevel, mhDpsO, ohDpsO, daChance, taChance, dwChance, skillDW, charClass);
+    let ttlDpsO = getTtlDPS()
 
-    let maxBS = getMaxBS(skillOff, charStr, mhDmg, skillBS, charClass);
+    let maxBS = getMaxBS()
 
-    document.querySelector("#dmgMod").value = dmgMod;
-    document.querySelector("#dmgBonus").value = dmgBonus;
-    document.querySelector("#dwChance").value = dwChance;
-    document.querySelector("#daChance").value = daChance;
-    document.querySelector("#taChance").value = taChance;
+    document.querySelector("#dmgMod").value = dmgMod
+    document.querySelector("#dmgBonus").value = dmgBonus
+    document.querySelector('#hasteCap').value = `${(hasteCap * 100)}%`
+    document.querySelector("#dwChance").value = `${(dwChance * 100).toFixed(2)}%`
+    document.querySelector("#daChance").value = `${(daChance * 100).toFixed(2)}%`
+    document.querySelector("#taChance").value = `${(taChance * 100).toFixed(2)}%`
 
-    document.querySelector("#mhDmgCapO").value = mhDmgCapO;
-    document.querySelector("#mhMinO").value = mhMinO;
-    document.querySelector("#mhMaxO").value = mhMaxO;
-    document.querySelector("#mhDelayO").value = mhDelayO;
-    document.querySelector("#mhDpsO").value = mhDpsO;
+    document.querySelector("#mhDmgCapO").value = mhDmgCapO
+    document.querySelector("#mhMinO").value = mhMinO
+    document.querySelector("#mhMaxO").value = mhMaxO
+    document.querySelector("#mhDelayO").value = mhDelayO
+    document.querySelector("#mhDpsO").value = mhDpsO
 
-    document.querySelector("#ohDmgCapO").value = ohDmgCapO;
-    document.querySelector("#ohMinO").value = ohMinO;
-    document.querySelector("#ohMaxO").value = ohMaxO;
-    document.querySelector("#ohDelayO").value = ohDelayO;
-    document.querySelector("#ohDpsO").value = ohDpsO;
+    document.querySelector("#ohDmgCapO").value = ohDmgCapO
+    document.querySelector("#ohMinO").value = ohMinO
+    document.querySelector("#ohMaxO").value = ohMaxO
+    document.querySelector("#ohDelayO").value = ohDelayO
+    document.querySelector("#ohDpsO").value = ohDpsO
 
-    document.querySelector("#ttlDpsO").value = ttlDpsO;
+    document.querySelector("#ttlDpsO").value = ttlDpsO
 
-    document.querySelector("#maxBS").value = maxBS;
+    document.querySelector("#maxBS").value = maxBS
 
-    function getDmgBonus(level, thBonus, charClass) {
-        let bonus;
+    function getDmgBonus() {
+        let bonus
 
-        if (level < 28) {
-            bonus = 0;
+        if (charLevel < 28) {
+            bonus = 0
         } else if (thBonus > 0) {
-            bonus = thBonus;
+            bonus = thBonus
         } else if (charClass == "Bard" || charClass == "Monk" || charClass == "Paladin" || charClass == "Ranger" ||
             charClass == "Rogue" || charClass == "Shadow Knight" || charClass == "Warrior") {
-            bonus = (level - 25) / 3;
+            bonus = (charLevel - 25) / 3
         } else {
-            bonus = 0;
+            bonus = 0
         }
 
-        return Math.floor(bonus);
+        return Math.floor(bonus)
     }
 
-    function getDmgMod(offenseSkill, strength) {
-        let dmgMod = (parseFloat(offenseSkill) + parseFloat(strength)) / 100;
-        return dmgMod;
+    function getDmgMod() {
+        let dmgMod = (parseFloat(skillOff) + parseFloat(charStr)) / 100
+        return dmgMod
     }
 
-    function getWepDelay(wepDmg, wepDelay, haste, level) {
-        let delay;
-        haste = haste * .01;
+    function getWepDelay(wepDmg, wepDelay) {
+        let delay
+        haste = charHaste * .01
 
-        if (level < 31 && haste > .5) {
-            haste = .5;
-        } else if (level < 51 && haste > .74) {
-            haste = .74;
-        } else if (level < 60 && haste > .94) {
-            haste = .94;
-        } else if (level >= 60 && haste > 1) {
-            haste = 1;
+        if (wepDmg == 0 || wepDmg == '') {
+            return delay = 0
         }
 
-        delay = wepDelay / (1 + haste);
+        if (charLevel < 31 && haste > .5) {
+            haste = .5
+        } else if (charLevel < 51 && haste > .74) {
+            haste = .74
+        } else if (charLevel < 60 && haste > .94) {
+            haste = .94
+        } else if (charLevel >= 60 && haste > 1) {
+            haste = 1
+        }
+
+        delay = wepDelay / (1 + haste)
 
         if (delay < 5) {
-            delay = 5;
-        } else if (wepDmg == 0) {
-            delay = 0;
+            delay = 5
         }
 
-        delay = Math.round(delay);
-        return delay;
+        delay = Math.round(delay)
+        return delay
     }
 
-    function getDwChance(level, dwSkill, charClass) {
-        let dwChance;
+    function getHasteCap() {
+        if (charLevel < 31) {
+            haste = .5
+        } else if (charLevel < 51) {
+            haste = .74
+        } else if (charLevel < 60) {
+            haste = .94
+        } else if (charLevel >= 60) {
+            haste = 1
+        }
+        return haste
+    }
+
+    function getDwChance() {
+        let dwChance
 
         if (charClass == "Monk") {
-            dwChance = (parseFloat(level) + parseFloat(dwSkill)) / 400;
+            dwChance = (parseFloat(charLevel) + parseFloat(skillDW)) / 400
         } else {
-            dwChance = (parseFloat(level) + parseFloat(dwSkill)) / 500;
+            dwChance = (parseFloat(charLevel) + parseFloat(skillDW)) / 500
         }
 
-        return dwChance.toFixed(3);
+        return dwChance
     }
 
-    function getDaChance(daSkill) {
-        let daChance = daSkill / (400 * 1.05);
-        return daChance.toFixed(3);
+    function getDaChance() {
+        let daChance = skillDA / (400 * 1.05)
+        return daChance
     }
 
-    function getTaChance(level, daSkill, charClass) {
-        let taChance;
+    function getTaChance() {
+        let taChance
 
-        if (level >= 60 && charClass == "Monk") {
-            taChance = (daSkill / 2) / (400 * 1.05);
-        } else if (level >= 60 && charClass == "Warrior") {
-            taChance = (daSkill / 2) / (400 * 1.05);
+        if (charLevel >= 60 && charClass == "Monk") {
+            taChance = (skillDA / 2) / (400 * 1.05)
+        } else if (charLevel >= 60 && charClass == "Warrior") {
+            taChance = (skillDA / 2) / (400 * 1.05)
         } else {
-            taChance = 0;
+            taChance = 0
         }
 
-        return taChance.toFixed(3);
+        return taChance
     }
 
-    function getMainDPS(minDmg, maxDmg, delay) {
-        let dps;
+    function getMainDPS() {
+        let dps
 
-        dps = ((minDmg + maxDmg) / 2) / (delay * .1);
-        return dps.toFixed(3);
+        dps = ((mhMinO + mhMaxO) / 2) / (mhDelayO * .1)
+        return dps.toFixed(2)
     }
 
-    function getOffDPS(minDmg, maxDmg, delay, dwChance) {
-        let dps;
+    function getOffDPS() {
+        let dps
 
-        dps = (((minDmg + maxDmg) / 2) / (delay * .1)) * dwChance;
+        dps = (((ohMinO + ohMaxO) / 2) / (ohDelayO * .1)) * dwChance
 
-        if (minDmg == 0) {
-            dps = 0;
+        if (ohMinO == 0) {
+            dps = 0
         }
 
-        return dps.toFixed(3);
+        return dps.toFixed(2)
     }
 
-    function getTtlDPS(level, mhDPS, ohDPS, daChance, taChance, dwSkill, charClass) {
-        let totalDPS;
+    function getTtlDPS() {
+        let totalDPS
 
-        if (dwSkill < 150) {
-            totalDPS = (parseFloat(mhDPS) + (parseFloat(mhDPS) * parseFloat(daChance)) + parseFloat(ohDPS));
+        if (skillDW < 150) {
+            totalDPS = parseFloat(mhDpsO) + (parseFloat(mhDpsO) * parseFloat(daChance)) + parseFloat(ohDpsO)
         } else {
-            totalDPS = (parseFloat(mhDPS) + (parseFloat(mhDPS) * parseFloat(daChance))) + (parseFloat(ohDPS) + (ohDPS * daChance));
+            totalDPS = parseFloat(mhDpsO) + (parseFloat(mhDpsO) * parseFloat(daChance)) + parseFloat(ohDpsO) + (parseFloat(ohDpsO) * parseFloat(daChance))
         }
 
         if (charClass == "Warrior" || charClass == "Monk") {
-            if (level >= 60) {
-                totalDPS += (parseFloat(mhDPS) * parseFloat(taChance));
+            if (charLevel >= 60) {
+                totalDPS += parseFloat(mhDpsO) * parseFloat(taChance)
             }
         }
 
-        return parseFloat(totalDPS).toFixed(3);
+        return totalDPS.toFixed(2)
     }
 
-    function getMainMax(dmgMod, damage, twoHandBonus, bonus) {
-        let mainMax;
+    function getMainMax() {
+        let mainMax
 
         if (dmgMod < 2) {
-            dmgMod = 2;
+            dmgMod = 2
         }
 
-        if (twoHandBonus > 0) {
-            bonus = twoHandBonus;
+        if (thBonus > 0) {
+            dmgBonus = thBonus
         }
 
-        mainMax = (damage * dmgMod) + parseFloat(bonus);
-        mainMax = Math.floor(mainMax);
-        return mainMax;
+        mainMax = (mhDmg * dmgMod) + parseFloat(dmgBonus)
+        mainMax = Math.floor(mainMax)
+        return mainMax
     }
 
-    function getMainMin(bonus) {
-        let mainMin = bonus + 1;
-        return mainMin;
+    function getMainMin() {
+        let mainMin = dmgBonus + 1
+        return mainMin
     }
 
-    function getOffMin(wepDmg) {
-        let offMin;
+    function getOffMin() {
+        let offMin
 
-        if (wepDmg == 0) {
-            offMin = 0;
+        if (ohDmg == 0) {
+            offMin = 0
         } else {
-            offMin = 1;
+            offMin = 1
         }
 
-        return offMin;
+        return offMin
     }
 
-    function getOffMax(ohDmg, dmgMod) {
-        let offMax = ohDmg * dmgMod;
-        offMax = Math.floor(offMax);
-        return offMax;
+    function getOffMax() {
+        let offMax = Math.floor(ohDmg * dmgMod)
+        return offMax
     }
 
-    function getMaxBS(offenseSkill, strength, damage, bsSkill, charClass) {
-        let maxBS;
-        let strUnder;
-        let strOver = 0;
+    function getMaxBS() {
+        let maxBS
+        let strUnder
+        let strOver = 0
 
-        if (strength > 200) {
-            strUnder = 200;
-            strOver = 255 - strength;
+        if (charStr > 200) {
+            strUnder = 200
+            strOver = 255 - charStr
         } else {
-            strUnder = strength;
+            strUnder = charStr
         }
 
-        maxBS = ((parseFloat(offenseSkill) + parseFloat(strUnder)) + (parseFloat(strOver) / 5)) * parseFloat(damage) * (2 + (parseFloat(bsSkill) * .02)) / 100;
-        maxBS = Math.round(maxBS);
+        maxBS = ((parseFloat(skillOff) + parseFloat(strUnder)) + (parseFloat(strOver) / 5)) * parseFloat(mhDmg) * (2 + (parseFloat(skillBS) * .02)) / 100
+        maxBS = Math.round(maxBS)
 
         if (charClass !== "Rogue") {
-            maxBS = 0;
+            maxBS = 0
         }
 
-        return maxBS;
+        return maxBS
     }
 
-    function getDmgCap(level, wepDmg, charClass) {
-        let damage;
+    function getDmgCap(wepDmg) {
+        let damage
 
         if (charClass == "Enchanter" || charClass == "Magician" || charClass == "Necromancer" || charClass == "Wizard") {
-            if (level < 10 && wepDmg > 6) {
-                damage = 6;
-            } else if (level < 20 && wepDmg > 10) {
-                damage = 10;
-            } else if (level < 30 && wepDmg > 12) {
-                damage = 12;
-            } else if (level < 40 && wepDmg > 18) {
-                damage = 18;
-            } else if (level >= 40 && wepDmg > 20) {
-                damage = 20;
+            if (charLevel < 10 && wepDmg > 6) {
+                damage = 6
+            } else if (charLevel < 20 && wepDmg > 10) {
+                damage = 10
+            } else if (charLevel < 30 && wepDmg > 12) {
+                damage = 12
+            } else if (charLevel < 40 && wepDmg > 18) {
+                damage = 18
+            } else if (charLevel >= 40 && wepDmg > 20) {
+                damage = 20
             } else {
-                damage = wepDmg;
+                damage = wepDmg
             }
         }
 
         if (charClass == "Cleric" || charClass == "Druid" || charClass == "Shaman") {
-            if (level < 10 && wepDmg > 9) {
-                damage = 9;
-            } else if (level < 20 && wepDmg > 12) {
-                damage = 12;
-            } else if (level < 30 && wepDmg > 20) {
-                damage = 20;
-            } else if (level < 40 && wepDmg > 26) {
-                damage = 26;
-            } else if (level >= 40 && wepDmg > 40) {
-                damage = 40;
+            if (charLevel < 10 && wepDmg > 9) {
+                damage = 9
+            } else if (charLevel < 20 && wepDmg > 12) {
+                damage = 12
+            } else if (charLevel < 30 && wepDmg > 20) {
+                damage = 20
+            } else if (charLevel < 40 && wepDmg > 26) {
+                damage = 26
+            } else if (charLevel >= 40 && wepDmg > 40) {
+                damage = 40
             } else {
-                damage = wepDmg;
+                damage = wepDmg
             }
         }
 
         if (charClass == "Bard" || charClass == "Monk" || charClass == "Paladin" || charClass == "Ranger" ||
             charClass == "Rogue" || charClass == "Shadow Knight" || charClass == "Warrior") {
-            if (level < 10 && wepDmg > 10) {
-                damage = 10;
-            } else if (level < 20 && wepDmg > 20) {
-                damage = 20;
-            } else if (level < 30 && wepDmg > 30) {
-                damage = 30;
-            } else if (level < 40 && wepDmg > 60) {
-                damage = 60;
-            } else if (level >= 40 && wepDmg > 100) {
-                damage = 100;
+            if (charLevel < 10 && wepDmg > 10) {
+                damage = 10
+            } else if (charLevel < 20 && wepDmg > 20) {
+                damage = 20
+            } else if (charLevel < 30 && wepDmg > 30) {
+                damage = 30
+            } else if (charLevel < 40 && wepDmg > 60) {
+                damage = 60
+            } else if (charLevel >= 40 && wepDmg > 100) {
+                damage = 100
             } else {
-                damage = wepDmg;
+                damage = wepDmg
             }
         }
 
-        return damage;
+        return damage
     }
 }
